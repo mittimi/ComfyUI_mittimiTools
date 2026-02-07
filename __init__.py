8b5357d3dad42a1413f591274742405ba9464599
mittimi <mittings@gmail.com>
2026-02-07 16:14:57 +0900

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+@author: mittimi
+@title: mittimiTools
+@nickname: mittimiTools
+@description: Some tools.
+"""
+
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+WEB_DIRECTORY = "./js"
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
